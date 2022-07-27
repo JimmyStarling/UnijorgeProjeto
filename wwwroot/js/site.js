679e150de4945991312eab9166d7c7238947a17e
JimmyStarling <JimmyStarlingDev@gmail.com>
2022-07-27 11:37:52 -0300

    feature: Add mask to inputs via jquery-plugin-mask.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$(document).ready(function(){
+    $('.cpf').mask('000.000.000-00', {reverse: true});
+    $('.phone_with_ddd').mask('(00) 0000-0000');
+});
