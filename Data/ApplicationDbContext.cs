8047f4556be464a3c4368663f011201e70388999
JimmyStarling <JimmyStarlingDev@gmail.com>
2022-07-27 11:24:25 -0300

    Add DbSet of Usuarios.

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using UnijorgeCalouros.Models;
 
 namespace UnijorgeCalouros.Data;
 
@@ -9,4 +10,6 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+    public DbSet<Usuario> Usuarios { get; set; }
 }
