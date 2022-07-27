daf2781dd351e4221232ae732ebd3c91078323bf
JimmyStarling <JimmyStarlingDev@gmail.com>
2022-07-27 14:08:04 -0300

    Modify fields to nullable.

--- Models/Usuarios/Usuario.cs
+++ Models/Usuarios/Usuario.cs
@@ -13,23 +13,23 @@ namespace UnijorgeCalouros.Models
         [MinLength(2)]
         [MaxLength(200)]
         [Display(Name ="Nome Completo")]
-        public string NomeCompleto { get; set; }
+        public string? NomeCompleto { get; set; }
 
         [Required(ErrorMessage = "Email é obrigatório.")]
         [EmailAddress]
         [DisplayName("E-mail")]
-        public string Email { get; set; }
+        public string? Email { get; set; }
 
         [Required(ErrorMessage = "CPF é obrigatório.")]
         [DisplayFormat(DataFormatString = "{0:000\\.000\\.000-00}", ApplyFormatInEditMode = true)]
         [RegularExpression(@"^\d{3}\.\d{3}\.\d{3}-\d{2}$", 
          ErrorMessage = "Formato de CPF Inválido.")]
-        public string CPF { get; set; }
+        public string? CPF { get; set; }
 
         [Required(ErrorMessage = "Telefone é obrigatório.")]
         [RegularExpression(@"^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$", 
          ErrorMessage = "Telefone Inválido.")]
-        public string Telefone { get; set; }
+        public string? Telefone { get; set; }
 
         [Required(ErrorMessage = "Data é obrigatoria.")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
